0815135de7ac0d6d942537dcaa3a2cb2dc2e15b6
Yanna Lin <linyanna@yahoo.com>
2022-10-15 10:20:10 -0400

    commit lambda with webhook

---
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -0,0 +1,38 @@
+using System.Text;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace EsepWebhook;
+
+public class Function
+{
+  string environment_variable = Environment.GetEnvironmentVariable("slack_webhook");
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(object input, ILambdaContext context)
+    {
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, environment_variable)
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+            
+        return reader.ReadToEnd();
+    }
+}
